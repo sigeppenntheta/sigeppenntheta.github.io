71af2d1da25e5878e9f98270d7cb21bec5201a1e
“Taylor <taytabbbb@aol.com>
2019-03-18 03:30:09 -0400

    data

---
+++ push-app/js/google-sheets-html.js
@@ -0,0 +1,35 @@
+/*!
+ * 
+ * Google Sheets To HTML v0.9a
+ * 
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ * 
+ * The Google document's sharing must be set to public
+ * 
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1M1gYsdeBmMH2trlFE6XbWjArRKNufCRgOf7rG4duc4M&output=html&usp=sharing');
+    query.setQuery('SELECT B, C, D, E, F label B "Hill", C "Time", D "Buggy", E "Note"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
