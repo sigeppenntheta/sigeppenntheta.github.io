023521fe58baad35e6d59f4b49a64c6d3a9f51aa
Alex Barrios <ambarrio@andrew.cmu.edu>
2024-04-05 10:46:20 -0400

    Updated benda

---
+++ assets/js/benda.js
@@ -0,0 +1,7 @@
+function moveBenda() {
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    
+    document.getElementById("movingButton").style.left = x + "px";
+    document.getElementById("movingButton").style.top = y + "px";
+}
